a1c1de607dbb6ec553746c5fd7c22cb6c524df62
Theodore Keloglou <zf@sirodoht.com>
2023-09-17 16:45:14 +0100

    Remove nginx conf from deployment

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@
 # push origin
 git push
 
-# copy over nginx conf
-scp ./zfraenkel.com.conf root@evey.sirodoht.com:/etc/nginx/sites-available/
-
 # remove old and copy over new html files
 ssh root@evey.sirodoht.com rm -r /var/www/zfraenkel.com/
 ssh root@evey.sirodoht.com mkdir -p /var/www/zfraenkel.com/html
