547eba4e944931042951a31d4497fd1cb81e7e02
Theodore Keloglou <zf@sirodoht.com>
2024-11-21 16:25:38 +0000

    Add p on mkdir on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 
-ssh root@brick.01z.io 'mkdir /var/www/zfraenkel'
+ssh root@brick.01z.io 'mkdir -p /var/www/zfraenkel'
 
 scp index.html root@zfraenkel.com:/var/www/zfraenkel/index.html
 scp zfraenkel.caddy root@zfraenkel.com:/etc/caddy/zfraenkel.caddy
