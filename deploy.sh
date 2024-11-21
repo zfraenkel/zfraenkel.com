0441374d06779ff9f280f0bbb5d04a4e7a31997f
Theodore Keloglou <zf@sirodoht.com>
2024-11-21 16:27:20 +0000

    Change domain on deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ssh root@brick.01z.io 'mkdir -p /var/www/zfraenkel'
 
-scp index.html root@zfraenkel.com:/var/www/zfraenkel/index.html
-scp zfraenkel.caddy root@zfraenkel.com:/etc/caddy/zfraenkel.caddy
+scp index.html root@brick.01z.io:/var/www/zfraenkel/index.html
+scp zfraenkel.caddy root@brick.01z.io:/etc/caddy/zfraenkel.caddy
 
 ssh root@brick.01z.io 'systemctl restart caddy'
