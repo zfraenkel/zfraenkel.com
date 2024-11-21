5b69e5d974d6f56286f0f0649f226539325d1309
Theodore Keloglou <zf@sirodoht.com>
2024-11-21 16:22:49 +0000

    Change deployment to caddy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 #!/usr/local/bin/bash
 
-# push origin
-git push
+ssh root@brick.01z.io 'mkdir /var/www/zfraenkel'
 
-# remove old and copy over new html files
-ssh root@evey.sirodoht.com rm -r /var/www/zfraenkel.com/
-ssh root@evey.sirodoht.com mkdir -p /var/www/zfraenkel.com/html
-rsync -rsP ./index.html root@evey.sirodoht.com:/var/www/zfraenkel.com/html
-rsync -rsP ./style.css root@evey.sirodoht.com:/var/www/zfraenkel.com/html
+scp index.html root@zfraenkel.com:/var/www/zfraenkel/index.html
+scp zfraenkel.caddy root@zfraenkel.com:/etc/caddy/zfraenkel.caddy
 
-# restart nginx
-ssh root@evey.sirodoht.com 'nginx -t'
-ssh root@evey.sirodoht.com 'systemctl reload nginx'
+ssh root@brick.01z.io 'systemctl restart caddy'
